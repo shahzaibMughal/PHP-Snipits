09258edb3e2f07862dfbb39c5181d5240ae1b6aa
Zaheer <zaheer.mughal1965@gmail.com>
2019-04-29 11:32:31 +0500

    Signleton Snippet added

---
+++ Singleton.php
@@ -0,0 +1,22 @@
+<?php
+
+class Singleton{
+
+  protected static $instance;
+
+  public static function getInstance(){
+    if(null == static::$instance){
+      static::$instance = new static();
+    }
+    return static::$insstance;
+  }
+
+  //protected constructor prevent creating a new instance
+  protected function __construct(){}
+  // private clone prevent cloning of the instance
+  private function __clone(){}
+  // private wakeup prevent unserializing
+  private function __wakeup(){}
+}
+
+ ?>
